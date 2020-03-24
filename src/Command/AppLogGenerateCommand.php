c24e157d7099f8c02d3951ea0303d124d6caa0bf
Fernando Pradas <35688033+letnando@users.noreply.github.com>
2020-03-24 22:43:06 +0100

    Practice 2 - Monolog

---
+++ src/Command/AppLogGenerateCommand.php
@@ -0,0 +1,257 @@
+<?php
+
+namespace LaSalle\GroupZero\Command;
+
+use Faker\Factory;
+use Faker\Generator;
+use LaSalle\GroupZero\Command\Exception\Fake\CouldNotCreateUserException;
+use LaSalle\GroupZero\Command\Exception\Fake\ExpiredTokenException;
+use LaSalle\GroupZero\Command\Exception\Fake\UserNotFoundException;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+use Ramsey\Uuid\Uuid;
+use RuntimeException;
+use Symfony\Bundle\MakerBundle\Event\ConsoleErrorSubscriber;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Event\ConsoleEvent;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Contracts\Service\ServiceSubscriberInterface;
+use Symfony\Contracts\Service\ServiceSubscriberTrait;
+
+class AppLogGenerateCommand extends Command implements ServiceSubscriberInterface
+{
+    use ServiceSubscriberTrait;
+
+    protected static $defaultName = 'app:log:generate';
+
+    protected static $logLevels = [
+        LogLevel::EMERGENCY,
+        LogLevel::ALERT,
+        LogLevel::CRITICAL,
+        LogLevel::ERROR,
+        LogLevel::WARNING,
+        LogLevel::NOTICE,
+        LogLevel::INFO,
+        LogLevel::DEBUG,
+    ];
+
+    public static function getSubscribedServices(): array
+    {
+        return [
+            'request' => LoggerInterface::class,
+            'app'     => LoggerInterface::class,
+            'router'  => LoggerInterface::class,
+            'php'     => LoggerInterface::class,
+            'event'   => LoggerInterface::class,
+        ];
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Random log generator')
+            ->addOption(
+                'iterations',
+                'i',
+                InputOption::VALUE_REQUIRED,
+                'Number of logs to generate',
+                1000
+            )
+            ->addOption(
+                'delay',
+                'd',
+                InputOption::VALUE_REQUIRED,
+                'Time in microseconds between iterations',
+                0
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        dump($this->container);
+
+        $generator = Factory::create();
+
+        $delay = (int) $input->getOption('delay');
+
+        for ($i = (int) $input->getOption('iterations'); $i > 0; --$i) {
+            $logData = $this->randomLog($generator);
+
+            $this->getLogger($logData['channel'])->log($logData['level'], $logData['message'], $logData['context']);
+
+            usleep($delay);
+        }
+
+        return 0;
+    }
+
+    private function getLogger(string $channel): LoggerInterface
+    {
+        return $this->container->get($channel);
+    }
+
+    private function randomLog(Generator $generator): array
+    {
+        return $generator
+            ->randomElement(
+                [
+                    [
+                        'level'   => LogLevel::NOTICE,
+                        'channel' => 'app',
+                        'message' => 'User identified by "{user_id}" was not created due "{reason}"!',
+                        'context' => [
+                            'user_id'   => $this->randomUserId($generator),
+                            'reason'    => $generator->sentence(),
+                            'exception' => new CouldNotCreateUserException(),
+                        ],
+                    ],
+                    [
+                        'level'   => LogLevel::INFO,
+                        'channel' => 'app',
+                        'message' => 'User identified by "{user_id}" could not be found"!',
+                        'context' => [
+                            'user_id'   => $this->randomUserId($generator),
+                            'exception' => new UserNotFoundException(),
+                        ],
+                    ],
+                    [
+                        'level'   => LogLevel::INFO,
+                        'channel' => 'app',
+                        'message' => 'Added user role "{role}" to user identified by "{user_id}"',
+                        'context' => [
+                            'user_id' => $this->randomUserId($generator),
+                            'role'    => 'admin',
+                        ],
+                    ],
+                    [
+                        'level'   => LogLevel::CRITICAL,
+                        'channel' => 'request',
+                        'message' => sprintf(
+                            'Uncaught PHP Exception %s: "%s" at %s line %s',
+                            'PDOException',
+                            'Could not connect to database.',
+                            __FILE__,
+                            __LINE__
+                        ),
+                        'context' => [
+                            'exception' => new \PDOException(),
+                        ],
+                    ],
+                    [
+                        'level'   => LogLevel::NOTICE,
+                        'channel' => 'router',
+                        'message' => sprintf(
+                            'Unable to find the controller for path "%s". The route is wrongly configured.',
+                            'unknown/path'
+                        ),
+                        'context' => [
+                            'exception' => new NotFoundHttpException(),
+                        ],
+                    ],
+                    [
+                        'level'   => LogLevel::WARNING,
+                        'channel' => 'php',
+                        'message' => sprintf(
+                            'The stream or file "%s" could not be opened: '.'File not found!',
+                            '/var/www/var/cache/d1c126dd-a5c2-4ae4-b3ee-386c06fb2e05.pid'
+                        ),
+                        'context' => [
+                            'exception' => new BadRequestHttpException(),
+                        ],
+                    ],
+                    [
+                        'level'   => LogLevel::ERROR,
+                        'channel' => 'request',
+                        'message' => 'Noticed exception \'{exception_class}\' with message \'{file}:{line}',
+                        'context' => [
+                            'exception_class' => RuntimeException::class,
+                            'exception'       => new RuntimeException(),
+                            'file'            => __FILE__,
+                            'line'            => __LINE__,
+                        ],
+                    ],
+                    [
+                        'level'   => LogLevel::CRITICAL,
+                        'channel' => 'app',
+                        'message' => 'Token {id} is expired and cannot be used (iat: {iat})',
+                        'context' => [
+                            'id'        => (string) Uuid::uuid4(),
+                            'iat'       => time(),
+                            'exception' => new ExpiredTokenException(),
+                        ],
+                    ],
+                    [
+                        'level'   => LogLevel::ALERT,
+                        'channel' => 'request',
+                        'message' => 'Timeout of "{seconds} seconds" exceeded while trying to connect to host "{host}"',
+                        'context' => [
+                            'host'    => $generator->ipv4,
+                            'seconds' => $generator->numberBetween(0, 3000),
+                        ],
+                    ],
+                    [
+                        'level'   => LogLevel::DEBUG,
+                        'channel' => 'event',
+                        'message' => 'Listener "{listener}" was not called for event "{event}".',
+                        'context' => [
+                            'listener' => ConsoleErrorSubscriber::class,
+                            'event'    => ConsoleEvent::class,
+                        ],
+                    ],
+                    [
+                        'level'   => LogLevel::EMERGENCY,
+                        'channel' => 'app',
+                        'message' => 'Could not connect to AWS SQS.',
+                        'context' => [
+                            'listener' => ConsoleErrorSubscriber::class,
+                            'event'    => ConsoleEvent::class,
+                        ],
+                    ],
+                    [
+                        'level'   => LogLevel::ALERT,
+                        'channel' => 'app',
+                        'message' => 'Could not connect to AWS SQS.',
+                        'context' => [
+                            'listener' => ConsoleErrorSubscriber::class,
+                            'event'    => ConsoleEvent::class,
+                        ],
+                    ],
+                    [
+                        'level'   => LogLevel::INFO,
+                        'channel' => 'request',
+                        'message' => 'Matched route "{route}".',
+                        'context' => [
+                            'route'            => $generator->randomElement(
+                                ['hello', 'get_user', 'post_user', 'delete_user', 'patch_user']
+                            ),
+                            'route_parameters' => [],
+                            'request_uri'      => $generator->url,
+                            'method'           => $generator->randomElement(['GET', 'POST', 'PUT', 'PATCH', 'DELETE']),
+                        ],
+                    ],
+                ]
+            );
+    }
+
+    private function randomUserId(Generator $generator): string
+    {
+        return $generator->randomElement(
+            [
+                'b816b2b7-5fb7-48bc-b563-91ac9f6d1d71',
+                'b816b2b7-5fb7-48bc-b563-91ac9f6d1d71',
+                'b816b2b7-5fb7-48bc-b563-91ac9f6d1d71',
+                '22cbdcc5-da48-45b6-8289-28c7c5bb3db8',
+                '7e8f339f-4bf6-4223-abc2-d28bde8c5a11',
+                '7e8f339f-4bf6-4223-abc2-d28bde8c5a11',
+                'bec07183-71f3-4a12-b875-2b699fdfe9ad',
+                '46ce6f16-a2be-4112-bbdd-88575e9293a5',
+                'cd0aed4a-4669-4a51-af7a-2e6cfa34f660',
+                '9349fc89-252a-4838-9682-d72cf0d0858b',
+            ]
+        );
+    }
+}
--- src/Controller/HelloWorldController.php
+++ src/Controller/HelloWorldController.php
@@ -4,18 +4,60 @@ declare(strict_types = 1);
 
 namespace LaSalle\GroupZero\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 final class HelloWorldController extends AbstractController
 {
-    public function helloWorld(): Response
+    private const LOG_ERROR_QUERY_PARAM = 'logError';
+
+    /** @var string */
+    private $environment;
+
+    /** @var string */
+    private $environmentAlias;
+
+    public function __construct()
     {
+        $this->environment      = $_ENV['APP_ENV'];
+        $this->environmentAlias = $_ENV['APP_ENV_ALIAS'];
+    }
+
+    public function helloWorld(LoggerInterface $logger, Request $request): Response
+    {
+        $logger->info(
+            'Hi! This an info level log from {environment} environment',
+            [
+                'environment' => $this->environment,
+                'client_ips' => $request->getClientIps(),
+            ]
+        );
+
+        $logger->warning(
+            'Hi! This a warning level log from {environment} environment',
+            [
+                'environment' => $this->environment,
+                'client_ips' => $request->getClientIps(),
+            ]
+        );
+
+        if ($request->query->has(static::LOG_ERROR_QUERY_PARAM)) {
+            $logger->error(
+                'A request with {parameter} was made!',
+                [
+                    'parameter' => static::LOG_ERROR_QUERY_PARAM,
+                    'client_ips' => $request->getClientIps(),
+                ]
+            );
+        }
+
         return new Response(
             sprintf(
                 'Hello world from %s (%s) environment',
-                $_ENV['APP_ENV'],
-                $_ENV['APP_ENV_ALIAS']
+                $this->environmentAlias,
+                $this->environment
             )
         );
     }
